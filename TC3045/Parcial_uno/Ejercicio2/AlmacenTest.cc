228ceb69d36b8c87d281ba436b0b5dfade8f9cd6
EdgarGar <e_chif22@hotmail.com>
2017-01-13 11:10:28 -0600

    time

--- TC3045/Parcial_uno/Ejercicio2/AlmacenTest.cc
+++ TC3045/Parcial_uno/Ejercicio2/AlmacenTest.cc
@@ -1,13 +1,30 @@
 #include "gtest/gtest.h"
 #include "fixtures.h"
+#include "time.h"
 
-class AlmacenTest : public ::testing::Test{
+class TimeTest : public testing::Test
+{
 public:
+    virtual void SetUp(){
+        tiempo = time(NULL);
+    }
+    virtual void TearDown(){
+        EXPECT_TRUE((time(NULL) - tiempo) <= 1) << "Time Exceeded";
+    }
+private:
+    time_t tiempo;
+};
+
+class AlmacenTest : public TimeTest{
+public:
+
     static Almacen* ptr;
     void SetUp(){
+        TimeTest::SetUp();
         ptr->pushBack(1);
     }
     void TearDown(){
+        TimeTest::TearDown();
     }
     static void SetUpTestCase(){
        ptr =  new Almacen(10);
@@ -21,16 +38,19 @@ public:
 Almacen* AlmacenTest::ptr = nullptr;
 
 TEST_F(AlmacenTest, Constructor){
+    sleep(3);
     ASSERT_EQ(24, sizeof(*ptr));
 }
 
 TEST_F(AlmacenTest, Add){
+    sleep(3);
     ptr->pushBack(1);
     ASSERT_EQ(3, ptr->getSize());
 }
 
 
 TEST_F(AlmacenTest, erase){
+    sleep(3);
     ptr->pushBack(1);
     ptr->deleteAt(0);
     ASSERT_EQ(4, ptr->getSize());
