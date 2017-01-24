888e565bb8b18074f156f6b39680fde21f69ea46
EdgarGar <e_chif22@hotmail.com>
2017-01-24 10:51:25 -0600

    TypePara...

--- TC3045/Parcial_uno/Entregables/Evaluacion_uno/Ordenamiento.h
+++ TC3045/Parcial_uno/Entregables/Evaluacion_uno/Ordenamiento.h
@@ -1,16 +1,15 @@
 //Extraido de https://thilinasameera.wordpress.com/2011/06/01/sorting-algorithms-sample-codes-on-java-c-and-matlab/
-
 #include <iostream>
-
+using namespace std;
 
 template <class T>
 class Ordenamiento {
 public:
   static void bubbleSort(T [], int, bool(*) (T, T));
   static void insercionSort(T [], int, bool(*) (T, T));
-  static void seleccionSort(T [], int, bool(*) (T, T));
   static bool asc(T, T);
 };
+
 template <class T>
 bool Ordenamiento<T>::asc(T a, T b)
 {
@@ -23,12 +22,12 @@ void Ordenamiento<T>::bubbleSort(T v[] , int n, bool compara (T, T))
   T temp;
   for (int i = 0; i < n; i++)
   {
-    for (int j = n-1; j > i; j--)
+    for (int j = n - 1; j > i; j--)
     {
-      if ( compara(v[j-1], v[j]) )
+      if ( compara(v[j - 1], v[j]) )
       {
-        temp = v[j-1];
-        v[j-1] = v[j];
+        temp = v[j - 1];
+        v[j - 1] = v[j];
         v[j] = temp;
       }
     }
@@ -48,25 +47,6 @@ void Ordenamiento<T>::insercionSort(T v[], int n, bool compara (T, T))
       v[j + 1] = v[j];
       j--;
     }
-    v[j+1] = index;
-  }
-}
-
-template <class T>
-void Ordenamiento<T>::seleccionSort(T v[], int n, bool compara (T, T))
-{
-  int minimo = 0;
-  T temp;
-  for(int i = 0; i < n-1; i++)
-  {
-    minimo = i;
-    for(int j = i + 1; j < n; j++)
-    {
-      if (compara(v[minimo], v[j]) )
-      minimo = j;
-    }
-    temp = v[minimo];
-    v[minimo] = v[i];
-    v[i] = temp;
+    v[j + 1] = index;
   }
 }
